ec334d4f9b1ee418fea46cc1703217d3323011ff
Jason Horvath <jason.horvath@greaterdevelopment.com>
2018-12-29 02:47:03 -0500

    Basic working modal example that opens and closes

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -4,13 +4,20 @@ define(['domReady', 'jquery'], function(domReady, $) {
             selectors: {
                 container: '#easy-modal-container',
                 inner: '#easy-modal-inner',
-                autoPopulate: '#easy-modal-auto-populate'
+                autoPopulate: '#easy-modal-auto-populate',
+                show: '.easy-modal-show',
+                hide: '.easy-modal-hide',
+            },
+            css: {
+                isOpen: 'easy-modal-is-open'
             },
             frame: '<div id="easy-modal-container"><div id="easy-modal-shade"></div><div id="easy-modal-inner"></div></div>',
+            transitionSpeed: 300,
             _init: function() {
                 console.log('test');
                 self.addFrame();
                 self.autoPopulate();
+                self.bindControls();
             },
             addFrame: function() {
                 $('body').append($(self.frame));
@@ -23,11 +30,21 @@ define(['domReady', 'jquery'], function(domReady, $) {
             populate: function(contentElement) {
                 $(self.selectors.inner).html(contentElement.html());
             },
+            bindControls: function() {
+                $(self.selectors.show).on('click', function(e) {
+                    self.show();
+                });
+                $(self.selectors.hide).on('click', function(e) {
+                    self.hide();
+                });
+            },
             show: function() {
-                $(self.selectors.container).show();
+                $('body').addClass(self.css.isOpen);
+                $(self.selectors.container).fadeIn(self.transitionSpeed);
             },
             hide: function() {
-                $(self.selectors.container).hide();
+                $('body').removeClass(self.css.isOpen);
+                $(self.selectors.container).fadeOut(self.transitionSpeed);
             },
 
 
