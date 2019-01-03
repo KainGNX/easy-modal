c667920df5d88d36dd77039d359d007264290a6b
Jason Horvath <jason.horvath@greaterdevelopment.com>
2019-01-02 20:47:29 -0500

    Removed domReady from config and re minified

--- src/js/config/require-config.js
+++ src/js/config/require-config.js
@@ -1,7 +1,6 @@
 requirejs.config({
     "baseUrl": "js",
     "paths": {
-        "domReady": "https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady",
         "easy-modal": "modules/easy-modal.min",
         "jquery": "https://code.jquery.com/jquery-3.3.1.min"
     }
